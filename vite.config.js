61a87ac3ef85c3923c64cd233c629c1507a39bdb
MariaLBovin <maria.bovin@gmail.com>
2023-02-17 10:21:45 +0100

    json change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/portfolio-v2/",
+  base: '/portfolio-v2/',
   define: {
-    "process.env": {},
+    'process.env': {},
   },
 });
